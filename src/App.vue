f391457e0102c25c1fbb8118137c8d6a09eea02b
Marcelo Arnaldi <marcelo.arnaldi@legoolus.com.br>
2020-08-18 14:07:41 -0300

    Versão inicial com a estrutura de arquivos básica

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header title="Consumindo GitHub Api" />
+    <Logo />
+    <Menu />
+    <Content />
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Header   from './components/templates/Header'
+import Logo     from './components/templates/Logo'
+import Menu     from './components/templates/Menu'
+import Content  from './components/templates/Content'
+import Footer   from './components/templates/Footer'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: { Header, Menu, Content, Footer, Logo }  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+* {
+  font-family: Helvetica, Arial, sans-serif;
 }
+
+body {
+  margin: 0;
+}
+
+#app {
+  
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 80px 1fr 40px;
+  grid-template-columns: 150px 1fr;
+  grid-template-areas: 
+    "logo header"
+    "menu content"
+    "footer footer";
+}
+
 </style>
